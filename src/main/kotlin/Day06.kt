1f05591b2d31c32458cc1c9135d37fda59e8ff15
Jonathan Bergen <jonatbergn@gmail.com>
2023-12-06 12:47:02 +0100

    Day06

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,30 @@
+import Day.Companion.day
+
+fun main() = day(6) {
+
+    sample = """
+        Time:      7  15   30
+        Distance:  9  40  200
+    """.trimIndent()
+
+    part01 {
+        sampleResult = 288
+        solution = {
+            val (times, records) = map { it.numbers() }.toList()
+            times.zip(records).map { (time, record) -> wins(time, record) }.reduce(Int::times)
+        }
+        solutionResult = 2612736
+    }
+
+    part02 {
+        sampleResult = 71503
+        solution = {
+            val (time, record) = map { it.numbers().joinToString("").toLong() }.toList()
+            wins(time, record)
+        }
+        solutionResult = 29891250
+    }
+}
+
+private fun String.numbers() = split(' ').mapNotNull(String::toLongOrNull)
+private fun wins(time: Long, record: Long) = (1 until time).count { (time - it) * it > record }
